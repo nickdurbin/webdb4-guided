7e33111aca4eff8e9c831465055515e1125edfe2
ndurbin <nick.durbin@gmail.com>
2020-01-08 21:19:54 -0500

    Added  the pool value for foreign keys

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,11 @@ module.exports = {
     seeds: {
       directory: "./data/seeds",
     },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
 }
